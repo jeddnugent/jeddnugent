2abdb89e1074a81f37d8af0052adf04f61411d14
jeddnuget <jeddnugget@gmail.com>
2025-04-22 17:51:56 +1000

    Adding Creating and Deleting notes

--- React Projects/keep_clone/src/components/App.jsx
+++ React Projects/keep_clone/src/components/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  
+  var noteIndex = 0;
+
+  function addNote(newNote){
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote]
+    })
+  }
+
+  function deleteNote(id)
+  {
+    setNotes(prevNotes => {
+      return prevNotes.filter((note, index)=>{ return index !== id;})
+    })
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea addNote={addNote} id={notes.length}/>
+      {notes.map((note, index) => <Note key={index} id={index} title={note.title} content={note.content} deleteNote={deleteNote}/>)}
       <Footer />
     </div>
   );
--- React Projects/keep_clone/src/components/Note.jsx
+++ React Projects/keep_clone/src/components/Note.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Note(props){
-    return(
+    
+  
+  return(
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-      <button>DELETE</button>
+      <button onClick={()=>{props.deleteNote(props.id)}}>DELETE</button>
     </div>
   );
 }
